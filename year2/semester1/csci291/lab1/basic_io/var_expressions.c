869b96079ca4582e35d80e78fa2f42dbd9bac389
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 19:56:46 +0400

    Update var_expressions.c

--- year2/semester1/csci291/lab1/basic_io/var_expressions.c
+++ year2/semester1/csci291/lab1/basic_io/var_expressions.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 9;
+
+    printf("a = %d\n", a);
+
+
+    /*makes it negetive now a is -9*/
+    a =-a;
+    printf("a = %d\n", a);
+ 
+   /*subtracts a from a  so -9 -(-9)*/
+    a -= a;
+    printf("a = %d\n", a);
+
+   /*now a is 0 so --a subtracts it by 1  so output becomes -1*/
+    --a;
+    printf("a = %d\n", a);
+
+   /*a==a is gonna always be true so ans gonna be 1*/
+    a = (a == a);
+    printf("a = %d\n", a);
+
+    return 0;
+}
 

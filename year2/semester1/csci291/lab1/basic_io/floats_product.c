9d69bc98a21c8580786c2fbe89b0fca53abb3ba5
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 19:44:13 +0400

    Update floats_product.c

--- year2/semester1/csci291/lab1/basic_io/floats_product.c
+++ year2/semester1/csci291/lab1/basic_io/floats_product.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void) {
+    float a, b, product;
+
+    printf("enter first floatno : ");
+    scanf("%f", &a);
+
+    printf("enter second float no: ");
+    scanf("%f", &b);
+
+    product = a * b;
+    printf("you entered %.2f and %.2f, product = %.2f\n", a, b, product);
+
+    return 0;
+}

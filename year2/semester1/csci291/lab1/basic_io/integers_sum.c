85414e53518e568d3d3bf6738e9d737985d17091
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 19:43:32 +0400

    Update integers_sum.c

--- year2/semester1/csci291/lab1/basic_io/integers_sum.c
+++ year2/semester1/csci291/lab1/basic_io/integers_sum.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void) {
+    int x, y, sum;
+
+    printf("enter first no: ");
+    scanf("%d", &x);
+
+    printf("enter second no: ");
+    scanf("%d", &y);
+
+    sum = x + y;
+    printf("you entered %d and %d, sum = %d\n", x, y, sum);
+
+    return 0;
+}

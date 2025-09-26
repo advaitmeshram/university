3eea01eacbec257774ca68883c78380a971ba8eb
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 19:45:10 +0400

    Update char_twice.c

--- year2/semester1/csci291/lab1/basic_io/char_twice.c
+++ year2/semester1/csci291/lab1/basic_io/char_twice.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    char ch;
+
+    printf("enter a character: ");
+    scanf(" %c", &ch); 
+
+    printf("char printed twice: %c %c\n", ch, ch);
+
+    return 0;
+}
 

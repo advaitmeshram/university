d16a8568a24664f7bc4e83cc66451e565fe54873
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 18:38:59 +0400

    Update fahr_to_cels.c

--- year2/semester1/csci291/lab1/fahr_to_cels.c
+++ year2/semester1/csci291/lab1/fahr_to_cels.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h> /* give access to the stdio.h library */
+#define CONV_FACTOR (5.0/9) /* definition of a constant */
+int main(void)
+{
+float celsTemp; /* celsius - variable declaration */
+float fahrTemp; /* fahrenheit - variable declaration */
+printf("Enter the temperature in Fahrenheit:");
+scanf("%f", &fahrTemp );
+/*FOR PART B CHANGING IT TO printf("Wrong type: %d\n", celsTemp); , 
+ERROR BECAUSE %d expects an integer, but celsTemp is a float. */
+celsTemp = CONV_FACTOR * (fahrTemp - 32); /*convert to celsius */
+printf("Celsius temperature =%.1f \n" , celsTemp);
+  /* FOR PART B You can change it to:printf("Two decimals: %.2f\n", celsTemp);*/
+return 0;
+}
+1

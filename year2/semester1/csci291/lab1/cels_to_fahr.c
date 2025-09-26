513022df0b83625bd3739d129b94c732155c6caf
advaitmeshram <125113919+advaitmeshram@users.noreply.github.com>
2025-09-26 18:43:30 +0400

    Update cels_to_fahr.c

--- year2/semester1/csci291/lab1/cels_to_fahr.c
+++ year2/semester1/csci291/lab1/cels_to_fahr.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(void) {
+    float celsTemp;   /* Celsius temperature */
+    float fahrTemp;   /* Fahrenheit temperature */
+
+    printf("Enter temp in celsius: ");
+    scanf("%f",&celsTemp);
+
+    fahrTemp = 1.82 *celsTemp +32;
+    printf("Temp in fahrenheit = %.1f\n",fahrTemp);
+
+    return 0;
+}
+/*OUTPUT
+Enter temp in celsius: 0
+Temp in fahrenheit = 32.0*/
